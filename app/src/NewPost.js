68d1b45425a10832efc3ff80d74c2a88961067e6
Levhita <levhita@gmail.com>
2018-03-21 17:52:27 -0600

    empty validation

--- app/src/NewPost.js
+++ app/src/NewPost.js
@@ -5,24 +5,46 @@ class NewPost extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value:"",
+      privacy: "private",
+      disabled: "disabled",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePrivacyChange = this.handlePrivacyChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
     return (
       <div className="new_post">
         <h1>Nueva Publicación</h1>
-        <textarea/>
+        <textarea onChange={this.handleChange} value={this.state.value} placeholder="¿Qué estás pensando?"/>
         <div className="post_options">
-          <select name="privacy">
-            <option>Amigos</option>
-            <option>Público</option>
+          <select value={this.state.privacy} onChange={this.handlePrivacyChange}>
+            <option value="private">Amigos</option>
+            <option value="public">Público</option>
           </select>
-          <input type="button" value="Publicar"/>
+          <div className="error">{this.state.error}</div>
+          <input disabled={this.state.value<=0?"disabled":""} type="button" onClick={this.handleSubmit} value="Publicar"/>
         </div>
       </div>
     );
   }
+
+  handleChange(event) {
+    this.setState({ value:event.target.value });
+  }
+
+  handlePrivacyChange(event) {
+    this.setState({ privacy:event.target.value });
+  }
+  
+  handleSubmit() {
+    console.log(this.state);
+  }
+
 }
 
 export default NewPost;
